a97076885f92dddce372d356537bd7bd24082c79
muhammad.rafiuddin4287 <muhammad.rafiuddin4287@gmail.com>
2024-06-12 22:43:38 +0000

    belajar math relational dan logical operator

--- bin/hello_world.dart
+++ bin/hello_world.dart
@@ -4,8 +4,14 @@ void main(List<String> arguments) {
   double number1 = 7;
   double number2 = 3;
 
-  double hasil =  ++number1 + ++number2;
+  bool hasil1 =  ++number1 > ++number2;
+  bool hasil2 =  ++number1 < ++number2;
+  bool hasil3 =  ++number1 == ++number2;
+  bool hasil4 = (hasil1 && hasil2) && (hasil3 && hasil2);
 
-  print(hasil);
+  print(hasil1);
+  print(hasil2);
+  print(hasil3);
+  print(hasil4);
 
 }
