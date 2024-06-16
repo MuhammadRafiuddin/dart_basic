13b7a0ff5369625009cfd2179cce69db758ea82b
muhammad.rafiuddin4287 <muhammad.rafiuddin4287@gmail.com>
2024-06-16 22:25:43 +0000

    belajar null safety opertor

--- bin/hello_world.dart
+++ bin/hello_world.dart
@@ -1,14 +1,49 @@
 import 'dart:io';
 
+//1. null assertion operator (!)
+//2. if-null operator (??)
+//3. if-null assignment operator (??=)
+
+// Fungsi dari nullable tipe adalah memastikan data sebelum digunakan.
+
 void main(List<String> arguments){
 
-  print("SILAHKAN MASUKAN NAMA ANDA : ");
-  String? input = stdin.readLineSync();
+// 1. Contoh dari penggunaan null assertion opertor
+// String string = "123";
 
-  if(input == "muhammad rafiuddin"){
-    print("Input yang anda masukan benar");
-  }else{
-    print("Input yang anda masukan salah");
-  }
+// int? number = int.tryParse(string);
+
+// int result = number! + 2 ;
+
+// print(result);
+// jika program ini dijalankan maka hasilnya adalah 125 tidak terjadi error karena kita pastikan
+// nilainya pasti ada.
+
+// >>Contoh penggunaan if-null operator 
+
+// String string = "asd";
+// String string = "123";
+
+// int? number = int.tryParse(string);
+
+// int result = (number ?? 1) + 2 ; 
+
+// print(result);
+
+// >>Contoh penggunaan if-null assignment opertor 
+
+String string = "123";
+
+int? number = int.tryParse(string);
+
+number ??= 1 ; 
+
+int result = number + 2 ; 
+
+print(result);
+
+
+
+
 
 }
