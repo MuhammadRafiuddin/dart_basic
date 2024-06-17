85f55e74ef11947622066c6ca7c21a38a34ebb65
muhammad.rafiuddin4287 <muhammad.rafiuddin4287@gmail.com>
2024-06-17 12:31:44 +0000

    coba PBO dart

--- bin/hello_world.dart
+++ bin/hello_world.dart
@@ -1,48 +1,14 @@
 import 'dart:io';
 
-//1. null assertion operator (!)
-//2. if-null operator (??)
-//3. if-null assignment operator (??=)
+void main(List<String> Arguments){
 
-// Fungsi dari nullable tipe adalah memastikan data sebelum digunakan.
+int number = int.tryParse(stdin.readByteSync());
 
-void main(List<String> arguments){
-
-// 1. Contoh dari penggunaan null assertion opertor
-// String string = "123";
-
-// int? number = int.tryParse(string);
-
-// int result = number! + 2 ;
-
-// print(result);
-// jika program ini dijalankan maka hasilnya adalah 125 tidak terjadi error karena kita pastikan
-// nilainya pasti ada.
-
-// >>Contoh penggunaan if-null operator 
-
-// String string = "asd";
-// String string = "123";
-
-// int? number = int.tryParse(string);
-
-// int result = (number ?? 1) + 2 ; 
-
-// print(result);
-
-// >>Contoh penggunaan if-null assignment opertor 
-
-String string = "123";
-
-int? number = int.tryParse(string);
-
-number ??= 1 ; 
-
-int result = number + 2 ; 
-
-print(result);
-
-
+if(number > 0){
+  print("bilangan positif");
+}else{
+  print("bilangan negatif");
+}
 
 
 
