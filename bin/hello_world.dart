17a75763687fc8f0785cb9331bc71bdad5c7d216
muhammad.rafiuddin4287 <muhammad.rafiuddin4287@gmail.com>
2024-06-21 15:44:30 +0000

    paham switch expretion

--- bin/hello_world.dart
+++ bin/hello_world.dart
@@ -1,17 +1,15 @@
 import 'dart:io';
 
-void main(List<String> arguments){
-
-String? input = stdin.readLineSync() ?? "no input";
-
-int? number = int.tryParse(input) ?? 0;
-
-if(number > 0){
-  print("positif");
-}else if(number < 0){
-  print("Negativ");
-}else{
-  print("zero");
-}
-
+void main(List<String> arguments) {
+  String? input = stdin.readLineSync();
+  int out = switch (input) {
+    'satu' => 1,
+    'dua'  => 2,
+    'tiga' => 3,
+    'empat'=> 4,
+    'lima' => 5,
+    'enam' => 6,
+    _      => 5050,
+    };
+  print(out);
 }
