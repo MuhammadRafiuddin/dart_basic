6fd9d0a3ca7dfd032a8c1caa50db56a524261305
muhammad.rafiuddin4287 <muhammad.rafiuddin4287@gmail.com>
2024-06-24 23:57:03 +0000

    menggunakan interpolasi

--- bin/hello_world.dart
+++ bin/hello_world.dart
@@ -5,6 +5,6 @@ void main(List<String> arguments){
 stdout.write('silahkan masukan anga desimal anda : ');
 double angka = double.tryParse(stdin.readLineSync() ?? "0") ?? 0;
 
-print('hasilnya adalah : ' + angka.toStringAsFixed(2));
+print('hasilnya adalah :   ${angka.toStringAsFixed(2)}');
 
 }
