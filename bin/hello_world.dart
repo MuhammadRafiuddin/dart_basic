7ea3f4f5f086e0c67ff2f38f190666f156624733
muhammad.rafiuddin4287 <muhammad.rafiuddin4287@gmail.com>
2024-06-26 22:28:33 +0000

    latihan luas dan keliling persegi panjang

--- bin/hello_world.dart
+++ bin/hello_world.dart
@@ -1,10 +1,19 @@
 import 'dart:io';
 
+//tugas kedua membuat program menghitung luas
+//dan keliling persegi panjang
+
 void main(List<String> arguments){
 
-stdout.write('silahkan masukan anga desimal anda : ');
-double angka = double.tryParse(stdin.readLineSync() ?? "0") ?? 0;
+print("SELAMAT DATANG DI PROGRAM MENGHITUNG LUAS DAN KELILING SEGI EMPAT");
+stdout.write("silahkan input nilai lebar : ");
+double lebar = double.tryParse(stdin.readLineSync() ?? '0') ?? 0;
+stdout.write("silahkan input nilai panjang : ");
+double panjang = double.tryParse(stdin.readLineSync() ?? '0') ?? 0;
 
-print('hasilnya adalah :   ${angka.toStringAsFixed(2)}');
+double luas = panjang * lebar;
+double keliling = 2 * (panjang * lebar);
+print('Luas persegi panjang adalah : ${luas.toStringAsFixed(2)}');
+print('Keliling persegi panjang adalah : ${keliling.toStringAsFixed(2)}');
 
 }
